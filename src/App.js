8ca963afb25287ce9d9b7cbb603f4d8f385758b7
KJ <kj.theprog@gmail.com>
2021-09-18 22:35:47 -0300

    header & footer created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+
 import Routes from './Routes';
 
-const App = () => {
+const App = (props) => {
     return (
         <BrowserRouter>
+            <Header />
+
             <Routes />
+
+            <Footer />
         </BrowserRouter>
     );
 }
